743ec3db4b86a956b6726877dfb62739cab6cff9
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-07-21 16:53:54 +0200

    added pug, sass and gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+//const nodemon = require('gulp-nodemon');
+
+gulp.task('default', ['sass:watch', 'sass']);
+ 
+gulp.task('sass', function () {
+  return gulp.src('./public/scss/app.scss')
+    .pipe(sass({outputStyle: 'compress'}).on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./public/scss/**/*.scss', ['sass']);
+});
+
+/*gulp.task('startNodemon', function () {
+    nodemon({
+      script: 'app.js'
+    , ext: 'js pug'
+    , env: { 'NODE_ENV': 'development' }
+    })
+});
+*/
