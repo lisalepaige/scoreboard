467ac9672c4d800d8b4b10a0392405869c0b81c9
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-07-22 14:42:52 +0200

    add routes

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ mongoose.connect('mongodb://localhost/nodedemo');
 
 app.use(bodyParser.json());
 app.use("/", require('./routes/index'));
-app.use("/messages", require('./routes/messages'));
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
