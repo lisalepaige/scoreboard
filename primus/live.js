c1ac9bf6ea192b23358bc1abd69d4cbf88f618a9
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-07-24 17:35:14 +0200

    layout pug

---
+++ primus/live.js
@@ -0,0 +1,16 @@
+exports.go = function(server) {
+    
+    const Primus = require('primus');
+    let primus = new Primus(server, {});
+
+    primus.save(__dirname+'/primuslib.js');
+
+    primus.on("connection", function(primusspark) { 
+        console.log("spark primus connected");
+
+        primusspark.on("data", function(data) {
+            primus.write(data);
+        });
+    } );
+}
+
