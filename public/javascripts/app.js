a1fde5c32d2c22fee0b5be7bb38a08ea6c433da5
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-08-12 11:15:32 +0200

    plus min counter

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -8,11 +8,40 @@ primus = Primus.connect(url, {
     }
 });
 
+var aantal1 = document.querySelector(".aantalscore__team1");
+var teller1 = aantal1.innerHTML;
+
+var aantal2 = document.querySelector(".aantalscore__team2");
+var teller2 = aantal2.innerHTML;
+
+var aantal3 = document.querySelector(".aantalshots__team1");
+var teller3 = aantal3.innerHTML;
+
+var aantal4 = document.querySelector(".aantalshots__team2");
+var teller4 = aantal4.innerHTML;
+
+var aantal5 = document.querySelector(".aantalfouls__team1");
+var teller5 = aantal5.innerHTML;
+
+var aantal6 = document.querySelector(".aantalfouls__team2");
+var teller6 = aantal6.innerHTML;
+
+
 document.querySelector(".minscore__team1").addEventListener("click", function(e) {
     var minbutton = document.querySelector(".minscore__team1");
 
     primus.write(minbutton);
     console.log("min score 1 clicked");
+
+    if(teller1 <= 1)
+    {
+        teller1 = 0;
+        aantal1.innerHTML = teller1;
+    }
+    else {
+        teller1 -= 1;
+        aantal1.innerHTML = teller1;
+    }
 
     e.preventDefault();
     
@@ -24,6 +53,16 @@ document.querySelector(".minscore__team2").addEventListener("click", function(e)
     primus.write(minbutton);
     console.log("min score 2 clicked");
 
+    if(teller2 <= 1)
+    {
+        teller2 = 0;
+        aantal2.innerHTML = teller2;
+    }
+    else {
+        teller2 -= 1;
+        aantal2.innerHTML = teller2;
+    }
+
     e.preventDefault();
 });
 
@@ -33,6 +72,9 @@ document.querySelector(".plusscore__team1").addEventListener("click", function(e
     primus.write(plusbutton);
     console.log("plus score 1 clicked");
 
+        teller1++;
+        aantal1.innerHTML = teller1;
+    
     e.preventDefault();
 });
 
@@ -41,6 +83,9 @@ document.querySelector(".plusscore__team2").addEventListener("click", function(e
 
     primus.write(plusbutton);
     console.log("plus score 2 clicked");
+
+    teller2++;
+    aantal2.innerHTML = teller2;
 
     e.preventDefault();
 });
@@ -51,6 +96,19 @@ document.querySelector(".minshots__team1").addEventListener("click", function(e)
     primus.write(plusbutton);
     console.log("min shots 1 clicked");
 
+    primus.write(plusbutton);
+    console.log("min shots 2 clicked");
+
+    if(teller3 <= 1)
+    {
+        teller3 = 0;
+        aantal3.innerHTML = teller3;
+    }
+    else {
+        teller3 -= 1;
+        aantal3.innerHTML = teller3;
+    }
+
     e.preventDefault();
 });
 
@@ -59,6 +117,16 @@ document.querySelector(".minshots__team2").addEventListener("click", function(e)
 
     primus.write(plusbutton);
     console.log("min shots 2 clicked");
+
+    if(teller4 <= 1)
+    {
+        teller4 = 0;
+        aantal4.innerHTML = teller4;
+    }
+    else {
+        teller4 -= 1;
+        aantal4.innerHTML = teller4;
+    }
 
     e.preventDefault();
 });
@@ -69,6 +137,9 @@ document.querySelector(".plusshots__team1").addEventListener("click", function(e
     primus.write(plusbutton);
     console.log("plus shots 1 clicked");
 
+    teller3++;
+    aantal3.innerHTML = teller3;
+
     e.preventDefault();
 });
 
@@ -77,6 +148,9 @@ document.querySelector(".plusshots__team2").addEventListener("click", function(e
 
     primus.write(plusbutton);
     console.log("plus shots 2 clicked");
+
+    teller4++;
+    aantal4.innerHTML = teller4;
 
     e.preventDefault();
 });
@@ -87,6 +161,16 @@ document.querySelector(".minfouls__team1").addEventListener("click", function(e)
     primus.write(plusbutton);
     console.log("min fouls 1 clicked");
 
+    if(teller5 <= 1)
+    {
+        teller5 = 0;
+        aantal5.innerHTML = teller5;
+    }
+    else {
+        teller5 -= 1;
+        aantal5.innerHTML = teller5;
+    }
+
     e.preventDefault();
 });
 
@@ -95,6 +179,16 @@ document.querySelector(".minfouls__team2").addEventListener("click", function(e)
 
     primus.write(plusbutton);
     console.log("min fouls 2 clicked");
+
+    if(teller6 <= 1)
+    {
+        teller6 = 0;
+        aantal6.innerHTML = teller6;
+    }
+    else {
+        teller6 -= 1;
+        aantal6.innerHTML = teller6;
+    }
 
     e.preventDefault();
 });
@@ -105,6 +199,9 @@ document.querySelector(".plusfouls__team1").addEventListener("click", function(e
     primus.write(plusbutton);
     console.log("plus fouls 1 clicked");
 
+    teller5++;
+    aantal5.innerHTML = teller5;
+
     e.preventDefault();
 });
 
@@ -114,35 +211,35 @@ document.querySelector(".plusfouls__team2").addEventListener("click", function(e
     primus.write(plusbutton);
     console.log("plus fouls 2 clicked");
 
+    teller6++;
+    aantal6.innerHTML = teller6
+
     e.preventDefault();
 });
 
-
-
-
-
 /*function Min(data)
 {
-       if(teller <= 1)
+    if(teller <= 1)
     {
-        teller = 1;
+        teller = 0;
         aantal.innerHTML = teller;
-        Total();
     }
     else {
         teller -= 1;
         aantal.innerHTML = teller;
-        Total();
     }
 
 }
 
-plus.addEventListener("click", function(e){
-   
-    console.log(teller);
+function Plus(data)
+{
+    teller += 1;
+    aantal.innerHTML = teller;
+}
+*/
 
-    
-        
+/*plus.addEventListener("click", function(e){
+  
 });
 
 
