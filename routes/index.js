795058871cbc0ddb3279b537e6ec1a8be89936f3
Lisa Lepaige <lisa_lepaige@hotmail.com>
2018-08-19 10:42:31 +0200

    insert items mongodb

--- routes/index.js
+++ routes/index.js
@@ -29,19 +29,19 @@ router.get('/scoreboard', function (req, res) {
         var coll3 = dbo.collection("updates").find();  
         coll1.forEach(function (doc, err) {
             assert.equal(null, err);
-            console.log('--- Get items ---');
+            //console.log('--- Get items ---');
             console.log(doc);
             item = doc;
           },
           coll2.forEach(function (doc, err) {
             assert.equal(null, err);
-            console.log('--- Get items ---');
+            //console.log('--- Get items ---');
             console.log(doc);
             item = doc;
           }, 
           coll3.forEach(function (doc, err) {
             assert.equal(null, err);
-            console.log('--- Get items ---');
+            //console.log('--- Get items ---');
             console.log(doc);
             item = doc;
           }, function () {
@@ -55,10 +55,73 @@ router.get('/scoreboard', function (req, res) {
     });
 });
 
-router.get('/admin', function (req, res) {  
-    res.render('./admin', {
-        title: 'Control Panel'
+router.get('/admin', function (req, res) {
+    var item;
+    //connect
+    mongo.connect(online, function (err, db) {
+        console.log("connected");
+        assert.equal(null, err);
+        // get database
+        var dbo = db.db("scoreboard");
+        // get last item back 
+        var coll1 = dbo.collection("team1").find();  
+        var coll2 = dbo.collection("team2").find();
+        var coll3 = dbo.collection("updates").find();  
+        coll1.forEach(function (doc, err) {
+            assert.equal(null, err);
+            //console.log('--- Get items ---');
+            console.log(doc);
+            item = doc;
+          },
+          coll2.forEach(function (doc, err) {
+            assert.equal(null, err);
+            //console.log('--- Get items ---');
+            console.log(doc);
+            item = doc;
+          }, 
+          coll3.forEach(function (doc, err) {
+            assert.equal(null, err);
+            //console.log('--- Get items ---');
+            console.log(doc);
+            item = doc;
+          }, function () {
+            // callback -> after: close db, render get page with item
+            db.close();
+    
+            res.render('./admin', {
+                title: 'Control Panel'
+            });
+        })));
+    });   
+});
+
+router.post('/admin', function (req, res, next) {
+    var items1 = {
+        team1: req.body.team1,
+        score1: req.body.score1,
+        shots1: req.body.shots1,
+        fouls1: req.body.fouls1
+    };
+    var items2 = {
+        team2: req.body.team2,
+        score2: req.body.score2,
+        shots2: req.body.shots2,
+        fouls2: req.body.fouls2
+    };
+    var items3 = { update: req.body.update}
+
+    // connect to mongo db
+    mongo.connect(online, function (err, db) {
+    assert.equal(null, err);
+    // access database, use collection to insert item 
+    db.db('scoreboard').collection('team1').insertOne(items1, function (err, result) {
+      // callback (if no errors)
+      assert.equal(null, err);
+      console.log('--- Item inserted ---');
+      console.log(items1);
+      db.close();
     });
-})
+  });
+});
 
 module.exports = router;
